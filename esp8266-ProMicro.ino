c584daefb20c6df51bb9e7e055645a001bd6548b
Fattcat - but not fat cat <109801079+Fattcat@users.noreply.github.com>
2025-08-05 01:06:03 +0200

    Update esp8266-ProMicro.ino

--- esp8266-ProMicro.ino
+++ esp8266-ProMicro.ino
@@ -1,32 +1,77 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#include <WiFiUdp.h>
+#include <SoftwareSerial.h>
 
-const char* ssid = "RemoteControl";
-const char* password = "RemoteControlCursor";
-const char* host = "192.168.4.1";
+const char* ssid = "HID_WIFI";
+const char* password = "12345678";
+
+WiFiUDP udp;
+const int localPort = 1234;
+
+SoftwareSerial softSerial(D8, D7); // TX, RX
+
+const int LED_PIN = LED_BUILTIN;
+
+unsigned long lastWiFiCheck = 0;
+bool wasConnected = false;
 
 void setup() {
-  Serial.begin(9600); // Komunikácia s Pro Micro
-  WiFi.begin(ssid, password);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH); // LED vypnutá (aktívna LOW)
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
+  Serial.begin(115200);
+  softSerial.begin(9600);
+
+  connectToWiFi();
+  udp.begin(localPort);
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    http.begin("http://" + String(host) + "/");
-    int httpCode = http.GET();
-
-    if (httpCode == 200) {
-      String payload = http.getString();
-      Serial.println(payload); // Posielame Pro Micro
+  // === Kontrola WiFi stavu každých 1000 ms ===
+  if (millis() - lastWiFiCheck > 1000) {
+    lastWiFiCheck = millis();
+    if (WiFi.status() != WL_CONNECTED) {
+      if (wasConnected) {
+        Serial.println("WiFi connection lost. Reconnecting...");
+        wasConnected = false;
+      }
+      blinkWhileConnecting();
+      connectToWiFi();
     }
-
-    http.end();
   }
 
-  delay(50); // Rýchlosť aktualizácie
+  // === Spracovanie UDP packetov ===
+  if (WiFi.status() == WL_CONNECTED) {
+    int packetSize = udp.parsePacket();
+    if (packetSize) {
+      char buffer[128];
+      int len = udp.read(buffer, sizeof(buffer) - 1);
+      if (len > 0) {
+        buffer[len] = '\0';
+        softSerial.println(buffer);
+        Serial.println(buffer);
+      }
+    }
+  }
+}
+
+void connectToWiFi() {
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    blinkWhileConnecting();
+  }
+
+  Serial.println("\nConnected to ESP32");
+  digitalWrite(LED_PIN, LOW);  // LED zapnutá
+  wasConnected = true;
+}
+
+void blinkWhileConnecting() {
+  digitalWrite(LED_PIN, LOW);  // LED ON
+  delay(250);
+  digitalWrite(LED_PIN, HIGH); // LED OFF
+  delay(250);
 }
